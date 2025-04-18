fd99cc0cd359411eb124522c8f2a87de2634f0e5
KirillLv <log.k.vv@gmail.com>
2025-04-18 08:43:25 +0400

    Обязательно

--- WindowsFormsApp3/Program.cs
+++ WindowsFormsApp3/Program.cs
@@ -16,6 +16,11 @@ namespace GoldenSectionMinimization
         private Button btnCalculate;
         private Chart chart1;
         private TextBox txtPrecision;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
         private ComboBox cmbFunction;
 
         public MainForm()
@@ -69,35 +74,40 @@ namespace GoldenSectionMinimization
             this.btnCalculate = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.cmbFunction = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtA
             // 
-            this.txtA.Location = new System.Drawing.Point(53, 80);
+            this.txtA.Location = new System.Drawing.Point(88, 126);
             this.txtA.Name = "txtA";
             this.txtA.Size = new System.Drawing.Size(100, 20);
             this.txtA.TabIndex = 0;
             // 
             // txtB
             // 
-            this.txtB.Location = new System.Drawing.Point(160, 80);
+            this.txtB.Location = new System.Drawing.Point(88, 177);
             this.txtB.Name = "txtB";
             this.txtB.Size = new System.Drawing.Size(100, 20);
             this.txtB.TabIndex = 1;
             // 
             // txtPrecision
             // 
-            this.txtPrecision.Location = new System.Drawing.Point(53, 110);
+            this.txtPrecision.Location = new System.Drawing.Point(54, 246);
             this.txtPrecision.Name = "txtPrecision";
             this.txtPrecision.Size = new System.Drawing.Size(100, 20);
             this.txtPrecision.TabIndex = 2;
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(53, 140);
+            this.btnCalculate.Location = new System.Drawing.Point(51, 292);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(100, 30);
+            this.btnCalculate.Size = new System.Drawing.Size(137, 45);
             this.btnCalculate.TabIndex = 3;
             this.btnCalculate.Text = "Calculate";
             this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
@@ -116,14 +126,74 @@ namespace GoldenSectionMinimization
             // cmbFunction
             // 
             this.cmbFunction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbFunction.Location = new System.Drawing.Point(53, 50);
+            this.cmbFunction.Location = new System.Drawing.Point(55, 50);
             this.cmbFunction.Name = "cmbFunction";
             this.cmbFunction.Size = new System.Drawing.Size(200, 21);
             this.cmbFunction.TabIndex = 5;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(50, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Выберите метод:\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(50, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите диапазон:\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(50, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "A:\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(50, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "B:\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(49, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(190, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Введите точность:\r\n";
+            // 
             // MainForm
             // 
             this.ClientSize = new System.Drawing.Size(870, 650);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtA);
             this.Controls.Add(this.txtB);
             this.Controls.Add(this.txtPrecision);
